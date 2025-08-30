2477b2acd96cc172424317b9a71f12b86afce5c4
Raju donkeshwar <dvijaychandrakumar@gmail.com>
2025-08-30 15:48:19 +0530

    i added something

--- mysql.sh
+++ mysql.sh
@@ -12,6 +12,7 @@ LOG_FILE=$(echo $0 | cut -d "." -f1 )
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
+echo   " $(echo $0 | cut -d "." -f1 ) "
 echo " file name is $LOG_FILE-$TIMESTAMP.log "
 
 
